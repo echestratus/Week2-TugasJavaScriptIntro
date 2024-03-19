9c888abb9362585792ed2874aa6c8568d7972ae8
Farhan Nur Hakim <farhanz.nh.13@gmail.com>
2024-03-19 13:38:55 +0700

    Correcting the code in line 4

--- tugas3.js
+++ tugas3.js
@@ -1,9 +1,9 @@
 let printSegitiga = 7;
 let temp = "";
 
-for(let i=1; i<=7; i++){
+for(let i=1; i<=printSegitiga; i++){
     for(let j=1; j<=i; j++){
-        temp += j.toString();
+        temp += j;
     }
     console.log(temp);
     temp="";
